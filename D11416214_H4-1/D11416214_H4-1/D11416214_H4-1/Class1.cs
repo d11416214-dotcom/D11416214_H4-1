faefb057ccc7e2671e57befff1657cb3d1cb37c1
d11416214-dotcom <d11416214@gs.takming.edu.tw>
2026-03-27 10:04:03 +0800

    Add files via upload

---
+++ D11416214_H4-1/D11416214_H4-1/D11416214_H4-1/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Manager
+{
+	private readonly DateTime Startup;
+	public Manage()
+	{
+		Startup = DateTime.Now;
+	}
+	public DateTime GetStartup()
+	{
+		return Startup;
+	}
+}
---
+++ D11416214_H4-1/D11416214_H4-1/D11416214_H4-1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace D11416214_H4_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Manager n= new Manager();
+            Console.WriteLine(n.GetStartup().ToString());
+            //n.ToString = DateTime.UtcNow;
+            Console.WriteLine(n.GetStartup().ToString());
+        }
+    }
+}
